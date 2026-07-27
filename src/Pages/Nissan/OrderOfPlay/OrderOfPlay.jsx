b2b115c56bb37a9e2083b4f9d33572d996a44d18
Bhargavbaluni <balunibhargav@gmail.com>
2026-07-27 12:37:42 +0530

    Fix getUnassignedTeams

--- src/Pages/Nissan/OrderOfPlay/OrderOfPlay.jsx
+++ src/Pages/Nissan/OrderOfPlay/OrderOfPlay.jsx
@@ -61,23 +61,23 @@ function DraggableMatch({ match, time, allMatchesRef, isRemaining = false }) {
     disabled: isCompleted,
   });
 
-  console.log("========== MATCH ==========");
-  console.log("FULL MATCH OBJECT", match);
-  console.log("Match No:", match.matchNo);
-  console.log("Winner:", match.Winner);
-  console.log("TEAM1 =", match.Team1);
-  console.log("TEAM2 =", match.Team2);
-  console.log("Winner ID:", match.Winner?._id || match.Winner);
-  console.log("Team1 ID:", match.Team1?._id);
-  console.log("Team2 ID:", match.Team2?._id);
-  console.log(
-    "Team1 Winner:",
-    String(match.Winner?._id || match.Winner) === String(match.Team1?._id),
-  );
-  console.log(
-    "Team2 Winner:",
-    String(match.Winner?._id || match.Winner) === String(match.Team2?._id),
-  );
+  // console.log("========== MATCH ==========");
+  // console.log("FULL MATCH OBJECT", match);
+  // console.log("Match No:", match.matchNo);
+  // console.log("Winner:", match.Winner);
+  // console.log("TEAM1 =", match.Team1);
+  // console.log("TEAM2 =", match.Team2);
+  // console.log("Winner ID:", match.Winner?._id || match.Winner);
+  // console.log("Team1 ID:", match.Team1?._id);
+  // console.log("Team2 ID:", match.Team2?._id);
+  // console.log(
+  //   "Team1 Winner:",
+  //   String(match.Winner?._id || match.Winner) === String(match.Team1?._id),
+  // );
+  // console.log(
+  //   "Team2 Winner:",
+  //   String(match.Winner?._id || match.Winner) === String(match.Team2?._id),
+  // );
 
   const style = {
     ...(transform
